a4fa1ca1f3c6dddf16a8b0912b6f097941bb05b5
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-08-25 11:31:30 -0300

    feat: initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'semge/lista-telefonica',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0508f1a7b38b0d3ae045686833bcfdc5fc214de5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'semge/lista-telefonica' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0508f1a7b38b0d3ae045686833bcfdc5fc214de5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
