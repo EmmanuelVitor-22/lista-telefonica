82851ec6e526566d6ab5ecc73272887f1fac0abe
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-08-28 09:39:48 -0300

    feat: cria arquivo 'env.php' para centralizar os bancos de dados que poderão ser utilizados no sistema, e manter a seguraça, desacoplando o banco.

---
+++ config/Conection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+class Conection
+{
+
+    private $host= "127.0.0.1";
+    private $dbname= "lista_telefonica";
+    private $user = "root";
+    private $pass = "ojT2c3xXfrngZ43";
+
+    public function __construct()
+    {
+
+    }
+
+
+}
---
+++ env.php
@@ -0,0 +1,10 @@
+<?php
+    return [
+        'mysql' =>[
+            'host'=> '127.0.0.1',
+            'dbname' => 'lista_telefonica',
+            'user'=>'root',
+            'password' =>'ojT2c3xXfrngZ43'
+        ]
+    ];
+?>
