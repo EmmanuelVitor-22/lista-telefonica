53976456aa3b59ce15208fa1757ac72b79d97d45
Emmanuel Vitor <emmanuelvitor.ti@gmail.com>
2023-09-03 22:46:54 -0300

    feat: controller

--- app/Controller/ControllerContatos.php
+++ app/Controller/ControllerContatos.php
@@ -1,52 +1,43 @@
 <?php
 
+namespace App\Controller;
 
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-use App\Model\Contact;
 use App\Model\Address;
+use App\Model\Contact;
 use App\Model\Phone;
 
-try {
+class ControllerContatos
+{
 
-        $name = $_POST["name"] = 'a';
-        $email = $_POST["email"] = 'a';
-        $street = $_POST["street"] = 'a';
-        $number = $_POST["number"] = 'a';
-        $complement = $_POST["complement"] = 'a';
-        $zipCode = $_POST["zipCode"] = 'a';
-        $city = $_POST["city"] = 'a';
-        $state = $_POST["state"] = 'a';
-        $areaCode1 = $_POST["areaCode1"] = 'a';
-        $phoneNumber1 = $_POST["phoneNumber1"] = 'a';
-        $areaCode2 = $_POST["areaCode2"] = 'a';
-        $phoneNumber2 = $_POST["phoneNumber2"] = 'a';
+    public function cadastrarDados($dados)
+    {
 
         // Criar objetos de endereço e telefone
-        $contact = new Contact(null, $name, $email, new Address());
-        $address = new Address(null, $street, $number, $complement, $zipCode, $city, $state);
-        $phone = new Phone(null, $areaCode1, $phoneNumber1, $contact->getId());
-        $phone2 = new Phone(null, $areaCode2, $phoneNumber2, $contact->getId());
+        $address = new Address(null, $street = $dados['street'], $number = $dados['number'],
+            $complement = $dados['complement'], $zipCode = $dados['zipCode'], $city = $dados['city'],
+            $state = $dados['state']);
+
+        $contact = new Contact(null, $name = $dados['name'], $email= $dados['email'], $address);
+        $phone = new Phone(null, $areaCode1 = $dados['areaCode1'],
+            $phoneNumber1 = $dados['phoneNumber1'], $contact->getId());
+        $phone2 = new Phone(null, $areaCode2 = $dados['areaCode2'],
+            $phoneNumber2 = $dados['phoneNumber1'], $contact->getId());
 
         // Criar objeto de contato com endereço e telefone
         $contact->setPhones($phone);
         $contact->setPhones($phone2);
         $contact->setAddress($address);
 
-    // Inserir o contato no banco de dados
-    if ($contact->insertContato()) {
-        print_r($contact);
-        echo "Contato cadastrado com sucesso!";
+        // Inserir o contato no banco de dados
+        if ($contact->insertContato()) {
+            print_r($contact);
+            echo "Contato cadastrado com sucesso!";
 
-    } else {
-        echo "Erro ao cadastrar o contato.";
+        } else {
+            echo "Erro ao cadastrar o contato.";
+        }
+
     }
 
-}catch (\Exception $exception){
-    echo $exception->getMessage();
+
 }
-
-
-
-
-
--- app/Controller/controller.php
+++ app/Controller/controller.php
@@ -1,1 +1,37 @@
 <?php
+
+
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+use App\Controller\ControllerContatos;
+try {
+
+$dados = [
+        'name' => $_POST["name"],
+        'email' => $_POST["email"],
+        'street' => $_POST["street"],
+        'number' => $_POST["number"],
+        'complement' => $_POST["complement"],
+        'zipCode' => $_POST["zipCode"],
+        'city' => $_POST["city"],
+        'state' => $_POST["state"],
+        'areaCode1' => $_POST["areaCode1"],
+        'phoneNumber1' => $_POST["phoneNumber1"],
+        'areaCode2' => $_POST["areaCode2"],
+        'phoneNumber2' => $_POST["phoneNumber2"]
+
+    ];
+
+$controller = new ControllerContatos();
+$controller->cadastrarDados($dados);
+
+}catch (\Exception $exception){
+    echo $exception->getMessage();
+}
+
+
+
+
+
+
+
